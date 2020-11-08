5980677e86678da9d10294181b9d1be7e4836239
Kenny Levinsen <kl@kl.wtf>
2020-11-08 14:52:47 +0100

    Support basu as sd-bus impl

--- dbus.h
+++ dbus.h
@@ -1,10 +1,12 @@
 #ifndef _DBUS_H
 #define _DBUS_H
 
-#ifdef HAVE_SYSTEMD
-#include <systemd/sd-bus.h>
-#elif HAVE_ELOGIND
+#if defined(HAVE_BASU)
+#include <basu/sd-bus.h>
+#elif defined(HAVE_ELOGIND)
 #include <elogind/sd-bus.h>
+#elif defined(HAVE_SYSTEMD)
+#include <systemd/sd-bus.h>
 #endif
 
 #endif
